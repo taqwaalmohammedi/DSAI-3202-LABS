934eba7376787f71e4de3b61e547436113e2d793
student <student@vg-DSAI-3202-9.udst.internal>
2025-02-25 10:13:19 +0300

    add codes to the files

---
+++ src/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def load_data():
+    """
+    Loads the Iris dataset from an online source.
+
+    Returns:
+        pandas.DataFrame: The loaded dataset containing features and labels.
+    """
+    url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+    data = pd.read_csv(url)
+    return data
